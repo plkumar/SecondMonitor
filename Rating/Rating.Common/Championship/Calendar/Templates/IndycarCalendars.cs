2abaf5198fd3e37baa6ff8fce271848dc01d3a18
winzarten <matus.celko47@gmail.com>
2019-11-13 21:29:11 +0100

    indycar 2016

---
+++ Rating/Rating.Common/Championship/Calendar/Templates/IndycarCalendars.cs
@@ -0,0 +1,30 @@
+﻿namespace SecondMonitor.Rating.Common.Championship.Calendar.Templates
+{
+    using Tracks;
+
+    public static class IndycarCalendars
+    {
+        public static CalendarTemplateGroup AllIndyCar => new CalendarTemplateGroup("IndyCar", new CalendarTemplate[]{IndyCar2016});
+
+        public static CalendarTemplate IndyCar2016 => new CalendarTemplate("2016 - IndyCar", new []
+        {
+            new EventTemplate(TracksTemplates.StPetersburg0316, "Firestone Grand Prix of St. Petersburg"),
+            new EventTemplate(TracksTemplates.PhoenixInternationalRaceway, "Desert Diamond West Valley Phoenix Grand Prix"),
+            new EventTemplate(TracksTemplates.LongBeach, "42nd Toyota Grand Prix of Long Beach"),
+            new EventTemplate(TracksTemplates.BarberMotorsportsPark, "Honda Indy Grand Prix of Alabama"),
+            new EventTemplate(TracksTemplates.IndianapolisMotorSpeedwayRoadPresent, "Angie's List Grand Prix of Indianapolis"),
+            new EventTemplate(TracksTemplates.IndianapolisMotorSpeedwayOval, "100th Indianapolis 500 presented by PennGrade"),
+            new EventTemplate(TracksTemplates.DetroitBelleIsle, "Chevrolet Dual in Detroit Presented by Quicken Loan"),
+            new EventTemplate(TracksTemplates.DetroitBelleIsle, "Chevrolet Dual in Detroit Presented by Quicken Loan"),
+            new EventTemplate(TracksTemplates.RoadAmerica, "Kohler Grand Prix"),
+            new EventTemplate(TracksTemplates.IowaSpeedwayOval, "Iowa Corn 300"),
+            new EventTemplate(TracksTemplates.TorontoExhibitionPlace, "Honda Indy Toronto"),
+            new EventTemplate(TracksTemplates.MidOhioMainWithoutChicane, "Honda Indy 200"),
+            new EventTemplate(TracksTemplates.PoconoRacewayOval, "ABC Supply 500"),
+            new EventTemplate(TracksTemplates.TexasMotorSpeedwayOval, "Firestone 600"),
+            new EventTemplate(TracksTemplates.WatkinsGlenGpWithInnerLoop, "Grand Prix at The Glen"),
+            new EventTemplate(TracksTemplates.SonomaRacewayIndy, "GoPro Grand Prix of Sonoma"),
+
+        });
+    }
+}
--- Rating/Rating.Common/Championship/Calendar/Tracks/TracksTemplates.cs
+++ Rating/Rating.Common/Championship/Calendar/Tracks/TracksTemplates.cs
@@ -13,11 +13,15 @@
         public static TrackTemplate AVUSBerling = new TrackTemplate("AVUS (Berlin)", 8300);
 
 
+        public static TrackTemplate BarberMotorsportsPark = new TrackTemplate("Barber Motorsports Park", 3700);
+
         public static TrackTemplate BrandsHatchGpPresent = new TrackTemplate("Brands Hatch - Grand Prix (Present)", 3916);
         public static TrackTemplate BrandsHatchIndyPresent = new TrackTemplate("Brands Hatch - Indy (Present)", 1944);
 
         public static TrackTemplate BrnoPresent = new TrackTemplate("Brno - Grand Prix (Present)", 5403);
 
+
+        public static TrackTemplate DetroitBelleIsle = new TrackTemplate("Detroit Belle Isle", 3798);
 
         public static TrackTemplate DoningtonPark = new TrackTemplate("Donington Park - Grand Prix", 4020);
 
@@ -34,8 +38,16 @@
 
         public static TrackTemplate HungaroringPresent = new TrackTemplate("Hungaroring (Present)", 4381);
 
+
+        public static TrackTemplate IowaSpeedwayOval = new TrackTemplate("Iowa Speedway - Oval", 1408);
+
+        public static TrackTemplate IndianapolisMotorSpeedwayRoadPresent = new TrackTemplate("Indianapolis Motor Speedway - Road Course (Present)", 3925);
+        public static TrackTemplate IndianapolisMotorSpeedwayOval = new TrackTemplate("Indianapolis Motor Speedway - Oval", 4023);
+
         public static TrackTemplate IstanbulParkGp = new TrackTemplate("Istanbul Park - Grand Prix", 5333);
 
+
+        public static TrackTemplate LongBeach = new TrackTemplate("Long Beach", 3167);
 
         public static TrackTemplate LausitzringGpPresent = new TrackTemplate("EuroSpeedway Lausitzring - Grand Prix (Present)", 4345);
         public static TrackTemplate LausitzringDtmShortCoursePresent = new TrackTemplate("EuroSpeedway Lausitzring - DTM Short course(Present)", 3478);
@@ -50,6 +62,8 @@
         public static TrackTemplate MacauPresent = new TrackTemplate("Macau (Present)", 6115);
 
         public static TrackTemplate MarrakechPresent = new TrackTemplate("Marrakesh (Present)", 3000);
+
+        public static TrackTemplate MidOhioMainWithoutChicane = new TrackTemplate("Mid-Ohio - Main Circuit, without chicane", 3621);
 
         public static TrackTemplate MisanoWorldCircuitPresent = new TrackTemplate("Misano - World Circuit (Present)", 4200);
 
@@ -75,11 +89,19 @@
         public static TrackTemplate Oschersleben = new TrackTemplate("Oschersleben - Full Course", 3696);
         public static TrackTemplate Oschersleben00to06 = new TrackTemplate("Oschersleben - Full Course (2000 - 2006)", 3668);
 
+
+        public static TrackTemplate PoconoRacewayOval = new TrackTemplate("Pocono Raceway - Oval", 4023);
+
+        public static TrackTemplate PhoenixInternationalRaceway = new TrackTemplate("Phoenix International Raceway (2011 - 2018)", 1645);
+
+
         public static TrackTemplate RedBullRing = new TrackTemplate("Red Bull Ring - Grand Prix", 4318);
         public static TrackTemplate RedBullA1Ring = new TrackTemplate("A1 Ring - Grand Prix (1996 - 2004", 4326);
 
-        public static TrackTemplate Salzburgring7685 = new TrackTemplate("Salzburgring (1976 - 1985)", 4246);
+        public static TrackTemplate RoadAmerica = new TrackTemplate("Road America", 6514);
 
+
+        public static TrackTemplate Salzburgring7685 = new TrackTemplate("Salzburgring (1976 - 1985)", 4246);
 
         public static TrackTemplate SachsenringPresent = new TrackTemplate("Sachsenring (Present)", 3671);
 
@@ -87,15 +109,28 @@
 
         public static TrackTemplate SlovakiaRingTrack4 = new TrackTemplate("Slovakia Ring - Layout 4 (Present)", 5922);
 
+        public static TrackTemplate SonomaRacewayIndy = new TrackTemplate("Sonoma Raceway - IndyCar Circuit", 3838);
+
         public static TrackTemplate SpaPresent = new TrackTemplate("Spa-Francorchamps (2007 - Present), 7004 ");
 
         public static TrackTemplate SuzukaGPPresent = new TrackTemplate("Suzuka - Grand Prix (Present)", 5807);
         public static TrackTemplate SuzukaEastPresent = new TrackTemplate("Suzuka - East Circuit (Present)", 2243);
 
+        public static TrackTemplate StPetersburg0316 = new TrackTemplate("St. Petersburg (2003 - 2016)", 2903);
+        public static TrackTemplate StPetersburgPresent = new TrackTemplate("St. Petersburg (Present)", 2897);
+
+
+        public static TrackTemplate TexasMotorSpeedwayOval = new TrackTemplate("Texas Motor Speedway, Oval", 2414);
+
+        public static TrackTemplate TorontoExhibitionPlace = new TrackTemplate("Toronto, Exhibition Place", 2824);
+
         public static TrackTemplate TTAssenGpPresent = new TrackTemplate("TT Circuit Asses - Grand Prix (Present)", 4555);
 
 
         public static TrackTemplate VilaRealPresent = new TrackTemplate("Vila Real (Present)", 4600);
+
+
+        public static TrackTemplate WatkinsGlenGpWithInnerLoop = new TrackTemplate("Watkins Glen - Grand Prix, with inner loop.", 5552);
 
         public static TrackTemplate WunstorfAirfield = new TrackTemplate("Wunstorf Airfield", 5047);
 
